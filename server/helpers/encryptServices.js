519a656ff12535c3b5ad3da796fd4ad6b891bc7b
MrDrProfessorPatrick <vip.salm2@gmail.com>
2024-04-15 20:40:27 +0300

    decryptionStream added

--- server/helpers/encryptServices.js
+++ server/helpers/encryptServices.js
@@ -1,28 +1,24 @@
-const {
-  createCipheriv,
-  randomBytes,
-  scryptSync,
-} = require("node:crypto");
+const { createCipheriv, randomBytes, scryptSync } = require("node:crypto");
 
 function encryptSymetricService(textToEncrypt, key) {
-    const keySalt = scryptSync(key, "salt", 24);
-    const nonce = randomBytes(12);
-  
-    const cipher = createCipheriv("aes-192-ccm", keySalt, nonce, {
-      authTagLength: 16,
-    });
-  
-    let ciphertext = cipher.update(textToEncrypt, "utf8", "base64");
-    ciphertext += cipher.final("base64");
-    const tag = cipher.getAuthTag();
-    let nonceString = nonce.toString("base64");
-  
-    let tagString = tag.toString("base64");
+  const keySalt = scryptSync(key, "salt", 24);
+  const nonce = randomBytes(12);
 
-    console.log(nonceString.length, tagString.length);
-  
-    let responseCipher = nonceString + "-" + tagString + "-" + ciphertext + '&&';
-    return responseCipher;
-  }
+  const cipher = createCipheriv("aes-192-ccm", keySalt, nonce, {
+    authTagLength: 16,
+  });
 
-  module.exports = encryptSymetricService;
+  let ciphertext = cipher.update(textToEncrypt, "utf8", "base64");
+  ciphertext += cipher.final("base64");
+  const tag = cipher.getAuthTag();
+  let nonceString = nonce.toString("base64");
+
+  let tagString = tag.toString("base64");
+
+  console.log("LENGTH", nonceString.length, tagString.length); // 16 - 24
+
+  let responseCipher = nonceString + "-" + tagString + "-" + ciphertext + "&&";
+  return responseCipher;
+}
+
+module.exports = encryptSymetricService;
