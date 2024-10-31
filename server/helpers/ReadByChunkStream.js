35bd7ac8ae2218384ca86b035691c2cbc42fee0a
MrDrProfessorPatrick <vip.salm2@gmail.com>
2024-10-31 17:06:28 +0200

    31Oct17:06

--- server/helpers/ReadByChunkStream.js
+++ server/helpers/ReadByChunkStream.js
@@ -14,70 +14,96 @@ console.log(strHexB.toString("hex"));
 console.log("delimPos", delimPos);
 console.log("bitesQ", bitesQ.toString());
 
-class ChunkDevide extends Transform {
+class ChunkSplitter extends Transform {
   constructor(options){
     super(options)
     this.current = null;
-    this.next = null;
   }
 
   _transform(chunk, encoding, cb){
 
     function chunkHandler(chunk, current, next){
-
       if(chunk.length === 0) return; 
 
-      let delimPos = chunk.indexOf("|");
+      function findChunkByDelimiter(chunk){
+        let chunkCollector = Buffer.alloc()
+        let isChunkFull = false;
 
-      if(next){
-       this.push(current);
-       current = next;
-       next = null
-      }
+        let delimPos = chunk.indexOf("|");
 
-      if(!delimPos){
-        current+=chunk;
-        cb()
-        return;
-      }
+        // if delimPos = 0 you need to find next delimPos to know delimitedChunk
+        if(delimPos === 0){
+          let nextChunk = chunk.subarray(1)
+          return findChunkByDelimiter(nextChunk);
+        }
+  
+        if(delimPos > 0 && delimPos < chunk.length-1){
+          let prevChunk = chunk.subarray(0, delimPos+1);
+          let nextChunk = chunk.subarray(delimPos+1)
+          chunkCollector = prevChunk;
+          return findChunkByDelimiter(nextChunk)
 
-      if(delimPos > 0 && current != null){
-        let chunkToPush = current + chunk.subarray(0, delimPos+1);
-        current = null;
-        this.push(chunkToPush);
-        if(delimPos <= chunk.length) current = chunk.subarray(delimPos)
-        cb()
-        return  
-      }
 
-      if(delimPos > 0 && current === null){
-        let chunkToPush = chunk.subarray(0, delimPos+1);
-        this.push(chunkToPush);
-        if(delimPos <= chunk.length) current = chunk.subarray(delimPos);
-        cb()
-        return
-      }
-      
-      if(delimPos === 0 && current != null){
-        // adfadf + |asfasdf
-        this.push(current);
-        current = null;
-        let nextDelim = chunk.subarray(1).indexOf('|');
-        if(nextDelim){
-         current = chunk.subarray(1, nextDelim+1) 
-         next = chunk.subarray(nextDelim);
-        } else {
-          current = chunk.subarray(1);
-          next = null;
+        }
+  
+        if(delimPos === chunk.length-1){
+          let nextChunk = chunk.subarray(0);
         }
       }
 
-      if(delimPos === 0 && current != null){
-        this.push(this.current)
-        let nextDelim = chunk.subarray(1).indexOf('|');
-        nextDelim && this.push(this.current) 
-        !nextDelim && (current = chunk.subarray(1))
-      }
+
+
+
+
+      // if(next){
+      //  this.push(current);
+      //  current = next;
+      //  next = null
+      // }
+
+      // if(!delimPos){
+      //   current+=chunk;
+      //   cb()
+      //   return;
+      // }
+
+      // if(delimPos > 0 && current != null){
+      //   let chunkToPush = current + chunk.subarray(0, delimPos+1);
+      //   current = null;
+      //   this.push(chunkToPush);
+      //   if(delimPos <= chunk.length) current = chunk.subarray(delimPos)
+      //   cb()
+      //   return  
+      // }
+
+      // if(delimPos > 0 && current === null){
+      //   let chunkToPush = chunk.subarray(0, delimPos+1);
+      //   this.push(chunkToPush);
+      //   if(delimPos <= chunk.length) current = chunk.subarray(delimPos);
+      //   cb()
+      //   return
+      // }
+      
+      // if(delimPos === 0 && current != null){
+      //   // adfadf + |asfasdf
+      //   this.push(current);
+      //   current = null;
+      //   let nextDelim = chunk.subarray(1).indexOf('|');
+      //   if(nextDelim){
+      //    current = chunk.subarray(1, nextDelim+1) 
+      //    next = chunk.subarray(nextDelim);
+      //   } else {
+      //     current = chunk.subarray(1);
+      //     next = null;
+      //   }
+      // }
+
+      // if(delimPos === 0 && current != null){
+      //   this.push(this.current)
+      //   let nextDelim = chunk.subarray(1).indexOf('|');
+      //   nextDelim && this.push(this.current) 
+      //   !nextDelim && (current = chunk.subarray(1))
+      // }
     }
     
     chunkHandler(chunk, this.current)
