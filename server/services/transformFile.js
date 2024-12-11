e28392102acbb37c9cac75324d65180799375e40
MrDrProfessorPatrick <vip.salm2@gmail.com>
2024-12-11 13:48:32 +0200

    11dec13.48

--- server/services/transformFile.js
+++ server/services/transformFile.js
@@ -1,5 +1,5 @@
 const busboy = require('busboy');
-import { EventEmitter } from 'events'
+const EventEmitter = require('events');
 
 const TransformFile = require('../TransformMethods/TransformFile');
 
@@ -9,14 +9,12 @@ async function transformFile(req, res, next) {
   try {
       let fields = {};
       const bb = busboy({ headers: req.headers });
-      const errorEvent = new EventEmitter()
+      const errorEvent = new EventEmitter();
   
     function busboyErrorEventWrapper(){
+      console.log('busboyErrorEventWrapper')
       return new Promise((resolve, reject) => {
-        errorEvent.on('error', ()=>{
-          resolve();
-          return res.status(500).send('ERROR EVENT')
-        } );
+        errorEvent.once('error', resolve);
       })
     }  
 
@@ -42,7 +40,7 @@ async function transformFile(req, res, next) {
           if(extensionName === 'txt' && !compressionMethod && !encryptionMethod){
             // TODO 
             req.unpipe(bb);
-            return res.status(400).json('Choose the decompression method or file with extension like .br or .gz to decompress');
+            // return res.status(400).json('Choose the decompression method or file with extension like .br or .gz to decompress');
           }
       
             let transform = new TransformFile(
@@ -57,17 +55,20 @@ async function transformFile(req, res, next) {
           if(extensionName === 'br' || extensionName === 'gz'){
             let fileNameTxt = filename.replace(/\.\w+/, ".txt");
       
-            res.setHeader(
-              "Content-disposition",
-              `attachment; filename="${fileNameTxt}"`
-            );
+            // res.setHeader(
+            //   "Content-disposition",
+            //   `attachment; filename="${fileNameTxt}"`
+            // );
   
             transform.decompress(file, res).catch((error)=>{
               console.log('DECOMPRESS CATCH')
               req.unpipe(bb);
-              errorEvent.emit('error');
-              bb.emit('close');
+              // errorEvent.emit('error');
+              bb.emit('close')
+
             });
+
+            return res.status(400).json('Error after decompress')
             
           } else if(compressionMethod) {
             transform.compress(file, res);
@@ -90,14 +91,18 @@ async function transformFile(req, res, next) {
       console.log('ON FINISH EVENT')
       return res.status(500).send('DECOMPRESS FINISH EVENT')
     });
+ 
+
   
     req.pipe(bb);
+    // let result = await busboyErrorEventWrapper();
 
-    await busboyErrorEventWrapper();
+    //  console.log('sync result', result)
 
+    //  return res.status(500).json('ERROR OCCURED');
   } catch (error) {
     console.log('Error occured on file transformation', error);
-    return res.status(400).json('Error occured on file transformation');
+    // return res.status(400).json('Error occured on file transformation');
   }
 }
 
