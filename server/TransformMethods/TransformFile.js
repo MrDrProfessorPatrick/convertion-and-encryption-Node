c11b5d6df0dfca087af069ac6c536fdfc8c6d4dc
MrDrProfessorPatrick <vip.salm2@gmail.com>
2025-01-10 23:39:15 +0200

    Decompression type according extension done

--- server/TransformMethods/TransformFile.js
+++ server/TransformMethods/TransformFile.js
@@ -90,28 +90,21 @@ class TransformFile {
       const ac = new AbortController();
       const signal = ac.signal;
 
-      // let decompressionStream = new DecompressionStream({compressionType:'gz'})
       let decompressionStream = null;
-      // TODO change decomptession types
-      if(true) decompressionStream = zlib.createInflate();
-      // if(extensionName === 'br') decompressionStream = zlib.createBrotliDecompress();
+
+      if(extensionName === 'gz') decompressionStream = zlib.createInflate();
+      if(extensionName === 'br') decompressionStream = zlib.createBrotliDecompress();
       if(decompressionStream === null) throw new Error('No type of decompression was chosen')
       if(this.password){
-        console.log('this.password', this.password)
         let readableResult = await isDecompressionPossible(readable, this.compressionMethod, this.password);
-        console.log('readableResult', readableResult);
         let decryptSymetricSplitted = new DecryptSymetricSplittedStream({key:this.password});
-        let decryptionRes = await pipeline(readableResult, decryptSymetricSplitted, decompressionStream, writable);
-        console.log('decryptionRes', decryptionRes)
+        await pipeline(readableResult, decryptSymetricSplitted, decompressionStream, writable);
       } else {
         let readableResult = await isDecompressionPossible(readable);
-        console.log('readableResult', readableResult);
         if(!readableResult) throw new Error('Decompression is not possible');
         await pipeline(readableResult, decompressionStream, writable);
       }
     } catch (error) {
-      console.log(error, 'Error catched in decompress');
-      console.log('ACCCCCC')
       throw new Error(error)
     }
   }
